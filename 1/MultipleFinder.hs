4fef06c80f10691ae035b2c6dd97b26acd9aecbf
Adam Russell <adamlr6+pub@gmail.com>
2014-03-11 22:32:04 -0400

    Refactor problem 1.

--- 1/MultipleFinder.hs
+++ 1/MultipleFinder.hs
@@ -1,14 +1,14 @@
 module MultipleFinder where
 
+import qualified Control.Monad as M
 import qualified Data.List as L
 
-
 findMultiplesOf3And5Below :: Int -> [Int]
-findMultiplesOf3And5Below maxMultiple = L.sort . L.nub . concat $ do
+findMultiplesOf3And5Below maxMultiple = do
     let maxMultiple' = maxMultiple - 1
-    x <- [3,6..maxMultiple']
-    y <- [5,10..maxMultiple']
-    return [x, y]
+    x <- [1..maxMultiple']
+    M.guard $ x `mod` 3 == 0 || x `mod` 5 == 0
+    return x
 
 sumMultiplesOf3And5Below :: Int -> Int
 sumMultiplesOf3And5Below = sum . findMultiplesOf3And5Below
