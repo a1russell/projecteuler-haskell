3fd0ca8d552aa8a050c82ff77e9a164fd975a77f
Adam Russell <adamlr6+pub@gmail.com>
2014-06-03 22:53:49 -0400

    Refactor lambda in problem 3 primes function.

--- 3/PrimeFactors.hs
+++ 3/PrimeFactors.hs
@@ -3,13 +3,13 @@ module PrimeFactors where
 import qualified Data.List as List
 
 primes :: [Int]
-primes = List.nubBy (((>1).).gcd) [2..]
+primes = List.nubBy (\x y -> gcd x y > 1) [2..]
 
 primeFactors :: Int -> [Int]
 primeFactors n | n > 1 = go n primes
    where
      go n ps@(p:ps')
-       | p*p > n = [n]
+       | p * p > n = [n]
        | n `rem` p == 0 = p : go (n `quot` p) ps
        | otherwise = go n ps'
 
